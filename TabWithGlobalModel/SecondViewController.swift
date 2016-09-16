8f2ba3b762463565e6cfbc5bb4d1419c37f43098
wesleysadler <wesley_sadler@yahoo.com>
2016-09-16 13:22:41 -0500

    Initial Commit

---
+++ TabWithGlobalModel/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabWithGlobalModel
+//
+//  Created by Wesley Sadler on 9/16/16.
+//  Copyright © 2016 Digital Sadler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
